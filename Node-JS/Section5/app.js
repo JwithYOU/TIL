3b752d41989f1f54babf0836924cea82cce42796
YOU <khy960101@gmail.com>
2023-04-28 00:40:01 +0900

    파일 수정

--- Node-JS/Section5/app.js
+++ Node-JS/Section5/app.js
@@ -5,17 +5,6 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>'
-  );
-});
-
-app.post("/product", (req, res) => {
-  console.log(req.body);
-  res.redirect("/");
-});
-
 app.use("/", (req, res, next) => {
   res.send("<h1>Hello!</h1>");
 });
