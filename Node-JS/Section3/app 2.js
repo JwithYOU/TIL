76773b22c9a85655148c16dbe41d0a3da73367ed
YOU <khy960101@gmail.com>
2023-05-30 22:41:16 +0900

    2022-05-30 TIL

---
+++ Node-JS/Section3/app 2.js
@@ -0,0 +1,9 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method, req.headers);
+});
+
+server.listen(3000);
+//  그냥 createServer만으로는 서버를 실행시킬 수 없기 때문에 변수로 선언하고 listen 메서드를 사용
+// listen에 첫번째 인자로 들어가는 값은 포트번호
